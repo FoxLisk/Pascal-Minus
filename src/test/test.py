e77d403a2b2d9f69e8dc750d4c02af71e3a6c264
Alexander Corwin <ancorwin@gmail.com>
2013-07-26 03:48:58 -0400

    everything's now pretty injectable & callable so i can test things pretty easily going forward, I think. input isnt set up but i can automatedly verify that output works correctly. duck typed interfaces are delightful.

--- src/test/test.py
+++ src/test/test.py
@@ -1,3 +1,19 @@
 from pascalm import compile_pascal
 
-compile_pascal('source', 'dest', False, True)
+class CheatingStream:
+  def __init__(self):
+    self.__val = ''
+
+  def write(self, c):
+    self.__val += c
+
+  def reset(self):
+    self.__val = ''
+
+  def val(self):
+    return self.__val
+
+stream = CheatingStream()
+
+compile_pascal('write.pm', 'dest', False, True, stream)
+assert stream.val() == 'AA'
