70e7fcbe2f5baa9def1de86334aa3a35bcfdad34
Alexander Corwin <ancorwin@gmail.com>
2013-07-31 01:00:16 -0400

    Works with negative numbers now too

--- src/lib/writeint.pm
+++ src/lib/writeint.pm
@@ -1,31 +1,45 @@
 program writeint;
-procedure WriteInt(num : Integer);
-  var beginning, lastDigit : Integer;
+procedure Abs(var num : Integer);
   begin
-    lastDigit := num mod 10;
-    beginning := num div 10;
-    if beginning <> 0 then
-      WriteInt(beginning);
-    if lastDigit = 0 then
-      Write(48)
-    else if lastDigit = 1 then
-      Write(49)
-    else if lastDigit = 2 then
-      Write(50)
-    else if lastDigit = 3 then
-      Write(51)
-    else if lastDigit = 4 then
-      Write(52)
-    else if lastDigit = 5 then
-      Write(53)
-    else if lastDigit = 6 then
-      Write(54)
-    else if lastDigit = 7 then
-      Write(55)
-    else if lastDigit = 8 then
-      Write(56)
-    else if lastDigit = 9 then
-      Write(57);
+    if num < 0 then
+      num := -num;
+  end;
+
+procedure WriteInt(num : Integer);
+  procedure WritePos(posNum : Integer);
+    var beginning, lastDigit : Integer;
+    begin
+      lastDigit := posNum mod 10;
+      beginning := posNum div 10;
+      if beginning <> 0 then
+        WritePos(beginning);
+      if lastDigit = 0 then
+        Write(48)
+      else if lastDigit = 1 then
+        Write(49)
+      else if lastDigit = 2 then
+        Write(50)
+      else if lastDigit = 3 then
+        Write(51)
+      else if lastDigit = 4 then
+        Write(52)
+      else if lastDigit = 5 then
+        Write(53)
+      else if lastDigit = 6 then
+        Write(54)
+      else if lastDigit = 7 then
+        Write(55)
+      else if lastDigit = 8 then
+        Write(56)
+      else if lastDigit = 9 then
+        Write(57);
+    end;
+  begin
+    if num < 0 then
+      Write(45); {'-'}
+    Abs(num);
+    WritePos(num)
   end;
 begin
+  WriteInt(-1234567890)
 end.
